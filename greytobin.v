4ddf788f52ceac4b69bc7d368a9eb7bc9e040fb9
richardparker6969 <92014883+richardparker6969@users.noreply.github.com>
2021-10-08 08:25:48 +0530

    Update greytobin.v

--- greytobin.v
+++ greytobin.v
@@ -1,65 +1,28 @@
-$date
-	Sun May 23 19:23:40 2021
-$end
-$version
-	Icarus Verilog
-$end
-$timescale
-	1s
-$end
-$scope module gtob_test $end
-$var wire 4 ! b [3:0] $end
-$var reg 4 " g [3:0] $end
-$upscope $end
-$enddefinitions $end
-#0
-$dumpvars
-b0 "
-b0 !
-$end
-#1
-b1 !
-b1 "
-#2
-b11 !
-b10 "
-#3
-b10 !
-b11 "
-#4
-b111 !
-b100 "
-#5
-b110 !
-b101 "
-#6
-b100 !
-b110 "
-#7
-b101 !
-b111 "
-#8
-b1111 !
-b1000 "
-#9
-b1110 !
-b1001 "
-#10
-b1100 !
-b1010 "
-#11
-b1101 !
-b1011 "
-#12
-b1000 !
-b1100 "
-#13
-b1001 !
-b1101 "
-#14
-b1011 !
-b1110 "
-#15
-b1010 !
-b1111 "
-#16
+Verilog Code for Binary to Gray code conversion:
+
+module bin2gray
+        (input [3:0] bin, //binary input
+         output [3:0] G //gray code output
+        );
+
+//xor gates.
+assign G[3] = bin[3];
+assign G[2] = bin[3] ^ bin[2];
+assign G[1] = bin[2] ^ bin[1];
+assign G[0] = bin[1] ^ bin[0];
+
+endmodule
+
+Verilog Code for Gray code to Binary conversion:
+
+module gray2bin
+        (input [3:0] G, //gray code output
+         output [3:0] bin   //binary input
+        );
+
+assign bin[3] = G[3];
+assign bin[2] = G[3] ^ G[2];
+assign bin[1] = G[3] ^ G[2] ^ G[1];
+assign bin[0] = G[3] ^ G[2] ^ G[1] ^ G[0];
+
+endmodule
